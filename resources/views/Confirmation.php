fa0355e4421a0d03dd86d9d493b7c37fd518ad45
magdyksaleh <magdy@eduroam-int-dhcp-97-140-179.ucl.ac.uk>
2016-12-09 15:59:22 +0000

    Adding laravel projects

---
+++ resources/views/Confirmation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Confirmation</title>
+</head>
+<body>
+<h1> Congratulations your purchase has been succesful </h1>
+<p> your order number is ###########</p>
+<p><a href=HomePage.php> Click here to return to your homepage</a></p>
+  <!--Need the homepage link-->
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('LogIn');
+});
+
+
+Route::get('/RegisterPage.php', function () {
+    return view('RegisterPage');
+});
+
+
+Route::get('/HomePage.php', function () {
+    return view('HomePage');
+});
+
+Route::get('/LogIn.php', function () {
+    return view('LogIn');
+});
+
+Route::get('/BrowseEvents.php', function () {
+    return view('BrowseEvents');
+});
+
+Route::get('/CreateEvent.php', function () {
+    return view('CreateEvent');
+});
