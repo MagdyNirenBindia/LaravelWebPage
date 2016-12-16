dea3e4515be894f682de3a732e1cf568d8525c20
magdy <magdyksaleh@gmail.com>
2016-12-16 16:27:33 +0000

    minor mods

--- resources/views/TestHome.blade.php
+++ resources/views/TestHome.blade.php
@@ -7,6 +7,7 @@
 <a href="<?= URL::to('/logout') ?>">Log out</a>
 <a href="Example.blade.php">test</a>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<img src="<?php echo asset('images/4i.jpg')?>" alt="test image"/>
 <script src ="<?php echo asset('js/test.js')?>" type="text/javascript"></script>
 </body>
 </html>
