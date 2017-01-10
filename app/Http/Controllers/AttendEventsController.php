13b59de6238d0c7b869f0bd2717d318c913ae5b2
magdy <magdyksaleh@gmail.com>
2017-01-10 18:27:57 +0200

    attend button tweaks

---
+++ app/Http/Controllers/AttendEventsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Auth;
+use Session;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Schema;
+use App\User;
+use App\Event;
+use App\Attendee;
+use Illuminate\Database\Eloquent\Model;
+use View;
+use Illuminate\Support\Facades\Input;
+
+class AttendEventsController extends Controller
+{
+  public function doAttendEvent()
+  {
+    $attendee = new Attendee;
+    $attendee -> EventID = Input::get('eventID');
+    $attendee -> CustomerID = Input::get('customerID');
+    $attendee -> save();
+    return ($attendee);
+  }
+}
